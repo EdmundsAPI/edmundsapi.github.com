01f88e6ce239b9a209816d7634eb810e805155c3
Vera Kruhliakova <vera.kruhliakova@gmail.com>
2013-12-18 18:08:31 +0300

    TA108407: Setup testing environment

---
+++ test/test_cname.rb
@@ -0,0 +1,24 @@
+require 'test/unit'
+
+module Test
+  class TestCName < Test::Unit::TestCase
+
+    PRODUCTION_REPOSITORY = "edmundsapi.github.com"
+
+    def test_cname
+      repository = `git remote -v`
+
+      file = File.open("CNAME")
+      cname = file.read.gsub(/\s+/, "")
+      file.close
+
+      if repository.include? PRODUCTION_REPOSITORY
+        verify = cname == "developer.edmunds.com"
+      else
+        verify = repository.include? cname
+      end
+      assert(verify, "CNAME is not valid")
+    end
+
+  end
+end
