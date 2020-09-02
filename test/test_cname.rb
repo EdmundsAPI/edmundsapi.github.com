baa861f2a5894813f4d6412a8d84a108523b91c6
upliasau [`gitlab.shared-services.accounts.edmunds.com`] <upliasau@edmunds.com>
2020-09-02 10:01:23 +0300

    Updated test_cname.rb

--- test/test_cname.rb
+++ test/test_cname.rb
@@ -3,21 +3,14 @@ require 'test/unit'
 module Test
   class TestCName < Test::Unit::TestCase
 
-    PRODUCTION_REPOSITORY = "developer.edmunds.com"
+    PRODUCTION_HOST = "developer.edmunds.com"
 
     def test_cname
-      repository = `git remote -v`
-
       file = File.open("CNAME")
       cname = file.read.gsub(/\s+/, "")
       file.close
 
-      if repository.include? PRODUCTION_REPOSITORY
-        verify = cname == "developer.edmunds.com"
-      else
-        verify = repository.include? cname
-      end
-      assert(verify, "CNAME is not valid")
+      assert(cname == PRODUCTION_HOST, "CNAME is not valid")
     end
 
   end
